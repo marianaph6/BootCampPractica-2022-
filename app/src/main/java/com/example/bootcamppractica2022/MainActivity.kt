33fb3ed2cd6920f93792fee5b1b586b7676e765a
Mariana1996 <mariana1996.mph@gmail.com>
2022-05-22 20:59:58 -0500

    fun onStart login and trasnform icons social media with Glide

--- app/src/main/java/com/example/bootcamppractica2022/MainActivity.kt
+++ app/src/main/java/com/example/bootcamppractica2022/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.bootcamppractica2022
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.example.bootcamppractica2022.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_BootCampPractica2022)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Glide.with(this).load(R.drawable.ic_apple).apply(RequestOptions.circleCropTransform()).into(binding.imgIcApple)
+        Glide.with(this).load(R.drawable.ic_facebook).apply(RequestOptions.circleCropTransform()).into(binding.imgIcFacebook)
+        Glide.with(this).load(R.drawable.ic_google).apply(RequestOptions.circleCropTransform()).into(binding.imgIcGoogle)
     }
 }
