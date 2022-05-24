9cb6aa1a3754f2c36a1aa5d6a2d99e1318e9440c
Mariana1996 <mariana1996.mph@gmail.com>
2022-05-24 16:53:41 -0500

    Create uiPlay (Fragment and viewModel)

---
+++ app/src/main/java/com/example/bootcamppractica2022/ui/play/PlayFragment.kt
@@ -0,0 +1,42 @@
+package com.example.bootcamppractica2022.ui.play
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.bootcamppractica2022.databinding.FragmentPlayBinding
+
+class PlayFragment : Fragment() {
+
+    private var _binding: FragmentPlayBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val playViewModel =
+            ViewModelProvider(this).get(PlayViewModel::class.java)
+
+        _binding = FragmentPlayBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textPlay
+        playViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
