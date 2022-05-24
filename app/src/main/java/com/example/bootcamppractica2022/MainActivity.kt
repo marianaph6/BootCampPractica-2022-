6a66a39e980e16fdd5e1ef87f0f4464f7d6fcce5
Mariana1996 <mariana1996.mph@gmail.com>
2022-05-24 08:33:46 -0500

    Create fun onResume login_activity  (registration intent)

--- app/src/main/java/com/example/bootcamppractica2022/MainActivity.kt
+++ app/src/main/java/com/example/bootcamppractica2022/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.bootcamppractica2022
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -24,4 +26,20 @@ class MainActivity : AppCompatActivity() {
         Glide.with(this).load(R.drawable.ic_facebook).apply(RequestOptions.circleCropTransform()).into(binding.imgIcFacebook)
         Glide.with(this).load(R.drawable.ic_google).apply(RequestOptions.circleCropTransform()).into(binding.imgIcGoogle)
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        binding.txtRegistro.setOnClickListener(View.OnClickListener { toRegister() })
+    }
+
+    private fun toRegister() {
+        val intent = Intent(this, Registration::class.java)
+        startActivity(intent)
+    }
+
+    /*private fun toMovies() {
+        val intent = Intent(this, Registration::class.java)
+        startActivity(intent)
+    }*/
 }
