7300fcb4951daeb454c69b7bd064a739e6275519
Mariana1996 <mariana1996.mph@gmail.com>
2022-05-28 14:22:18 -0500

    Toolbar with icon arrow (registrationActivity)

--- app/src/main/java/com/example/bootcamppractica2022/RegistrationActiviyy.kt
+++ app/src/main/java/com/example/bootcamppractica2022/RegistrationActiviyy.kt
@@ -1,11 +1,36 @@
 package com.example.bootcamppractica2022
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bootcamppractica2022.databinding.ActivityRegistrationBinding
+
 
 class RegistrationActiviyy : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistrationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+        binding= ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.toolBarRegistration.setNavigationIcon(R.drawable.ic_arrow_back_24)
+
+        binding.toolBarRegistration.setNavigationOnClickListener(View.OnClickListener {
+            startActivity(
+                Intent(
+                    applicationContext,
+                    MainActivity::class.java
+                )
+            )
+        })
     }
 }
