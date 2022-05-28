0f73c70e0dc37223ab25de596bf3832926ad1f0b
Mariana1996 <mariana1996.mph@gmail.com>
2022-05-27 21:28:38 -0500

    Dependencies recyclerView Movie (itemView,data class, adapter,ViewHolder)

---
+++ app/src/main/java/com/example/bootcamppractica2022/Movie.kt
@@ -0,0 +1,9 @@
+package com.example.bootcamppractica2022
+
+data class Movie(
+    val url: String,
+    val title: String,
+    val rating: Float,
+    val year: Short,
+    val type: String
+)
---
+++ app/src/main/java/com/example/bootcamppractica2022/ui/search/MovieViewHolder.kt
@@ -0,0 +1,30 @@
+package com.example.bootcamppractica2022.ui.search
+
+import android.view.View;
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.bootcamppractica2022.Movie
+import com.example.bootcamppractica2022.databinding.ItemViewHolderMovieBinding
+import kotlin.math.roundToInt
+
+class MovieViewHolder (
+
+    view: View
+
+): RecyclerView.ViewHolder(view){
+
+    private val binding= ItemViewHolderMovieBinding.bind(view)
+
+    fun bin (model: Movie){
+        with(binding){
+            Glide.with(itemView).load(model.url).into((imageViewPrincipal))
+            textViewTitle.text=model.title
+            textViewYear.text=model.year.toString()
+            textViewType.text = model.type
+            ratingBar.numStars=model.rating.roundToInt()
+        }
+
+    }
+}
+
+
