f9ff96f0992e3077e21b61ade1776f27f21f53dd
Mariana1996 <mariana1996.mph@gmail.com>
2022-05-28 20:07:41 -0500

    Update content textViewActors (list with ",")

--- app/src/main/java/com/example/bootcamppractica2022/ui/search/MovieViewHolder.kt
+++ app/src/main/java/com/example/bootcamppractica2022/ui/search/MovieViewHolder.kt
@@ -1,11 +1,11 @@
 package com.example.bootcamppractica2022.ui.search
 
-import android.view.View;
+import android.text.TextUtils
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.bootcamppractica2022.Movie
 import com.example.bootcamppractica2022.databinding.ItemViewHolderMovieBinding
-import kotlin.math.roundToInt
 
 class MovieViewHolder (
 
@@ -20,7 +20,7 @@ class MovieViewHolder (
             Glide.with(itemView).load(model.url).into((imageViewPrincipal))
             textViewTitle.text=model.title
             textViewYear.text=model.year.toString()
-            textViewActors.text = model.actors.toString()
+            textViewActors.text = java.lang.String.join(",", model.actors)
             //ratingBar.numStars=model.rating.roundToInt()
         }
 
